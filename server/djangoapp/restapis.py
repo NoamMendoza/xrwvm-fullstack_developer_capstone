5cb794d8eb8da6bb74492e4f5d8c2e4f9c13f0d5
NoamMendoza <noamalfredo@gmail.com>
2026-01-16 15:17:38 -0500

    Linting

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -16,7 +16,7 @@ def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
-            params=params + key + "=" + value + "&"
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -29,6 +29,7 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print("Network exception occurred")
 
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -39,7 +40,7 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
-# def post_review(data_dict):
+
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
