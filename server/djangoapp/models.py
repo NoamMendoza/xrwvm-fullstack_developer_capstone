6d5c611ca1f3dae27e359e8256b5bbe5f3d5b7b0
NoamMendoza <noamalfredo@gmail.com>
2026-01-16 14:49:52 -0500

    Linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,9 +2,6 @@ from django.db import models
 #from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
-# Create your models here.
-
 class CarMake(models.Model):
 
     name = models.CharField(max_length=50)
