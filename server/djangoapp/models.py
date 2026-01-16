3ea7440c979c0eab32c7adfbb07dc27792771678
NoamMendoza <noamalfredo@gmail.com>
2026-01-16 14:45:23 -0500

    Linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,31 +1,24 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
 class CarMake(models.Model):
-# - Name
+
     name = models.CharField(max_length=50)
-# - Description
+
     description = models.TextField()
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+
     def __str__(self):
         return f"Name: {self.name}\nDescription: {self.description}"
 
 
 class CarModel(models.Model):
-# - Many-To-One relationship to Car Make model (One Car Make has many
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-# Car Models, using ForeignKey field)
-# - Name
     name = models.CharField(max_length=50)
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
+
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
@@ -38,15 +31,13 @@ class CarModel(models.Model):
         choices=CAR_TYPES,
         default='SEDAN'
     )
-# - Year (IntegerField) with min value 2015 and max value 2023
+
     year = models.IntegerField(
         validators=[
             MinValueValidator(2015),
             MaxValueValidator(2023)
         ]
     )
-# - Any other fields you would like to include in car model
     color = models.CharField(max_length=20, blank=True)
-# - __str__ method to print a car make object
     def __str__(self):
         return f"{self.car_make.name} {self.name} ({self.year})"
